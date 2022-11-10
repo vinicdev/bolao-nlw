7f3495b303598851292e642e4e19309a2911fa5c
Vinicius Cardoso <vinic8670@gmail.com>
2022-11-09 22:53:35 -0300

    Creating web/mobile project and database

---
+++ mobile/App.tsx
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Code 💻</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#181818',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    color: '#ffffff',
+    fontSize: 32,
+  }
+});
